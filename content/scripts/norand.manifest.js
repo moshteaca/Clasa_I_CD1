2b02f91d2d8d90c5dd7d02b178b74877b350ac1c
moshteaca <39875469+moshteaca@users.noreply.github.com>
2020-09-20 22:22:13 +0300

    Add CD files
    Initial commit

---
+++ content/scripts/norand.manifest.js
@@ -0,0 +1,381 @@
+// JavaScript Document
+var pageManifest = {
+	preface: 'data/pages/preface.html',
+	contents: 'data/pages/contents.html',
+	help: 'data/pages/help.html',
+	pages: [
+		{
+			url: 'data/pages/chapters/chapter_01_abc.html',
+			children: [
+				'data/pages/tasks/01_abc/02.html',
+				'data/pages/tasks/01_abc/03.html',
+				'data/pages/tasks/01_abc/04.html',
+				'data/pages/tasks/01_abc/05.html',
+				'data/pages/tasks/01_abc/06.html',
+				'data/pages/tasks/01_abc/07.html'
+			]
+		},
+		{
+			url: 'data/pages/chapters/chapter_02_a.html',
+			children: [
+				'data/pages/tasks/02_a/00.html',
+				'data/pages/tasks/02_a/01.html',
+				'data/pages/tasks/02_a/02.html',
+				'data/pages/tasks/02_a/03.html',
+				'data/pages/tasks/02_a/04.html',
+				'data/pages/tasks/02_a/05.html',
+				'data/pages/tasks/02_a/06.html',
+				'data/pages/tasks/02_a/07.html',
+				'data/pages/tasks/02_a/08.html'
+			]
+		},
+		{
+			url: 'data/pages/chapters/chapter_03_i_iacc.html',
+			children: [
+				'data/pages/tasks/03_i_iacc/00.html',
+				'data/pages/tasks/03_i_iacc/01.html',
+				'data/pages/tasks/03_i_iacc/02.html',
+				'data/pages/tasks/03_i_iacc/03.html',
+				'data/pages/tasks/03_i_iacc/04.html',
+				'data/pages/tasks/03_i_iacc/05.html',
+				'data/pages/tasks/03_i_iacc/07.html',
+				'data/pages/tasks/03_i_iacc/08.html',
+				'data/pages/tasks/03_i_iacc/09.html'
+			]
+		},
+		{
+			url: 'data/pages/chapters/chapter_04_o_oacc.html',
+			children: [
+				'data/pages/tasks/04_o_oacc/00.html',
+				'data/pages/tasks/04_o_oacc/01.html',
+				'data/pages/tasks/04_o_oacc/02.html',
+				'data/pages/tasks/04_o_oacc/03.html',
+				'data/pages/tasks/04_o_oacc/04.html',
+				'data/pages/tasks/04_o_oacc/05.html',
+				'data/pages/tasks/04_o_oacc/06.html',
+				'data/pages/tasks/04_o_oacc/07.html'
+			]
+		},
+		{
+			url: 'data/pages/chapters/chapter_05_m.html',
+			children: [
+				'data/pages/tasks/05_m/01.html',
+				'data/pages/tasks/05_m/02.html',
+				'data/pages/tasks/05_m/03.html',
+				'data/pages/tasks/05_m/04.html',
+				'data/pages/tasks/05_m/05.html',
+				'data/pages/tasks/05_m/06.html',
+				'data/pages/tasks/05_m/07.html',
+				'data/pages/tasks/05_m/08.html'
+			]
+		},
+		{
+			url: 'data/pages/chapters/chapter_06_s.html',
+			children: [
+				'data/pages/tasks/06_s/01.html',
+				'data/pages/tasks/06_s/02.html',
+				'data/pages/tasks/06_s/03.html',
+				'data/pages/tasks/06_s/04.html',
+				'data/pages/tasks/06_s/05.html',
+				'data/pages/tasks/06_s/06.html',
+				'data/pages/tasks/06_s/07.html',
+				'data/pages/tasks/06_s/08.html'
+			]
+		},
+		{
+			url: 'data/pages/chapters/chapter_07_t.html',
+			children: [
+				'data/pages/tasks/07_t/01.html',
+				'data/pages/tasks/07_t/02.html',
+				'data/pages/tasks/07_t/03.html',
+				'data/pages/tasks/07_t/04.html',
+				'data/pages/tasks/07_t/05.html',
+				'data/pages/tasks/07_t/06.html',
+				'data/pages/tasks/07_t/07.html',
+				'data/pages/tasks/07_t/08.html',
+				'data/pages/tasks/07_t/09.html',
+				'data/pages/tasks/07_t/10.html'
+			]
+		},
+		{
+			url: 'data/pages/chapters/chapter_08_v.html',
+			children: [
+				'data/pages/tasks/08_v/01.html',
+				'data/pages/tasks/08_v/02.html',
+				'data/pages/tasks/08_v/03.html',
+				'data/pages/tasks/08_v/04.html',
+				'data/pages/tasks/08_v/05.html',
+				'data/pages/tasks/08_v/06.html'
+			]
+		},
+		{
+			url: 'data/pages/chapters/chapter_09_recap_1.html',
+			children: [
+				'data/pages/tasks/09_recap1/02.html',
+				'data/pages/tasks/09_recap1/03.html',
+				'data/pages/tasks/09_recap1/04.html',
+				'data/pages/tasks/09_recap1/05.html',
+				'data/pages/tasks/09_recap1/06.html',
+				'data/pages/tasks/09_recap1/07.html',
+				'data/pages/tasks/09_recap1/08.html',
+				'data/pages/tasks/09_recap1/09.html',
+				'data/pages/tasks/09_recap1/10.html'
+			]
+		},
+		{
+			url: 'data/pages/chapters/chapter_10_test_1.html',
+			children: [
+				//'data/pages/tasks/10_test1/01.html'
+			]
+		},
+		{
+			url: 'data/pages/chapters/chapter_11_e.html',
+			children: [
+				'data/pages/tasks/11_e/01.html',
+				'data/pages/tasks/11_e/02.html',
+				'data/pages/tasks/11_e/03.html',
+				'data/pages/tasks/11_e/04.html',
+				'data/pages/tasks/11_e/05.html',
+				'data/pages/tasks/11_e/06.html',
+				'data/pages/tasks/11_e/07.html',
+				'data/pages/tasks/11_e/08.html',
+				'data/pages/tasks/11_e/09.html'
+			]
+		},
+		{
+			url: 'data/pages/chapters/chapter_12_l.html',
+			children: [
+				'data/pages/tasks/12_l/01.html',
+				'data/pages/tasks/12_l/02.html',
+				'data/pages/tasks/12_l/03.html',
+				'data/pages/tasks/12_l/04.html',
+				'data/pages/tasks/12_l/05.html',
+				'data/pages/tasks/12_l/06.html',
+				'data/pages/tasks/12_l/07.html',
+				'data/pages/tasks/12_l/08.html'
+			]
+		},
+		{
+			url: 'data/pages/chapters/chapter_13_u_uacc.html',
+			children: [
+				'data/pages/tasks/13_u_uacc/00.html',
+				'data/pages/tasks/13_u_uacc/01.html',
+				'data/pages/tasks/13_u_uacc/02.html',
+				'data/pages/tasks/13_u_uacc/03.html',
+				'data/pages/tasks/13_u_uacc/04.html',
+				'data/pages/tasks/13_u_uacc/05.html',
+				'data/pages/tasks/13_u_uacc/06.html',
+				'data/pages/tasks/13_u_uacc/07.html',
+				'data/pages/tasks/13_u_uacc/08.html',
+				'data/pages/tasks/13_u_uacc/09.html',
+				'data/pages/tasks/13_u_uacc/10.html',
+				'data/pages/tasks/13_u_uacc/11.html'
+			]
+		},
+		{
+			url: 'data/pages/chapters/chapter_14_p.html',
+			children: [
+				'data/pages/tasks/14_p/00.html',
+				'data/pages/tasks/14_p/01.html',
+				'data/pages/tasks/14_p/02.html',
+				'data/pages/tasks/14_p/03.html',
+				'data/pages/tasks/14_p/04.html',
+				'data/pages/tasks/14_p/05.html',
+				'data/pages/tasks/14_p/06.html',
+				'data/pages/tasks/14_p/07.html'
+			]
+		},
+		{
+			url: 'data/pages/chapters/chapter_15_aacc.html',
+			children: [
+				'data/pages/tasks/15_aacc/00.html',
+				'data/pages/tasks/15_aacc/01.html',
+				'data/pages/tasks/15_aacc/02.html',
+				'data/pages/tasks/15_aacc/03.html',
+				'data/pages/tasks/15_aacc/04.html',
+				'data/pages/tasks/15_aacc/05.html',
+				'data/pages/tasks/15_aacc/06.html',
+				'data/pages/tasks/15_aacc/07.html',
+				'data/pages/tasks/15_aacc/08.html',
+				'data/pages/tasks/15_aacc/09.html',
+				'data/pages/tasks/15_aacc/10.html'
+			]
+		},
+		{
+			url: 'data/pages/chapters/chapter_16_c.html',
+			children: [
+				'data/pages/tasks/16_c/01.html',
+				'data/pages/tasks/16_c/02.html',
+				'data/pages/tasks/16_c/03.html',
+				'data/pages/tasks/16_c/04.html',
+				'data/pages/tasks/16_c/05.html',
+				'data/pages/tasks/16_c/06.html',
+				'data/pages/tasks/16_c/07.html',
+				'data/pages/tasks/16_c/08.html'
+			]
+		},
+		{
+			url: 'data/pages/chapters/chapter_17_k.html',
+			children: [
+				'data/pages/tasks/17_k/00.html',
+				'data/pages/tasks/17_k/01.html',
+				'data/pages/tasks/17_k/02.html',
+				'data/pages/tasks/17_k/03.html',
+				'data/pages/tasks/17_k/04.html',
+				'data/pages/tasks/17_k/05.html',
+				'data/pages/tasks/17_k/06.html',
+				'data/pages/tasks/17_k/07.html',
+				'data/pages/tasks/17_k/08.html',
+				'data/pages/tasks/17_k/09.html'
+			]
+		},
+		{
+			url: 'data/pages/chapters/chapter_18_f.html',
+			children: [
+				'data/pages/tasks/18_f/01.html',
+				'data/pages/tasks/18_f/02.html',
+				'data/pages/tasks/18_f/03.html',
+				'data/pages/tasks/18_f/04.html',
+				'data/pages/tasks/18_f/05.html',
+				'data/pages/tasks/18_f/07.html',
+				'data/pages/tasks/18_f/08.html'
+			]
+		},
+		{
+			url: 'data/pages/chapters/chapter_19_recap_2.html',
+			children: [
+				'data/pages/tasks/19_recap2/02.html',
+				'data/pages/tasks/19_recap2/03.html',
+				'data/pages/tasks/19_recap2/04.html',
+				'data/pages/tasks/19_recap2/05.html',
+				'data/pages/tasks/19_recap2/06.html',
+				'data/pages/tasks/19_recap2/07.html',
+				'data/pages/tasks/19_recap2/08.html'
+			]
+		},
+		{
+			url: 'data/pages/chapters/chapter_20_test_2.html',
+			children: [
+				//'data/pages/tasks/20_test2/01.html',
+			]
+		}
+	]
+	
+};
+var pageTitles = {
+	contents: {
+		type: 'system',
+		icons: ['data/images/contents/icon_book_tn.png'],
+		label: 'Ábécéskönyv - 1. rész'
+	},
+	chapter_abc: {
+		type: 'system',
+		icons: ['data/images/contents/icon_abc_tn.png'],
+		label: 'Ábécés-mese'
+	},
+	chapter_a: {
+		type: 'vowel',
+		icons: ['data/images/contents/icon_a_tn.png'],
+		label: 'a, A'
+	},
+	chapter_i_iacc: {
+		type: 'vowel',
+		icons: ['data/images/contents/icon_i_tn.png','data/images/contents/icon_iacc_tn.png'],
+		label: 'i, I, í, Í'
+	},
+	chapter_o_oacc: {
+		type: 'vowel',
+		icons: ['data/images/contents/icon_o_tn.png','data/images/contents/icon_oacc_tn.png'],
+		label: 'o, O, ó, Ó'
+	},
+	chapter_m: {
+		type: 'consonant',
+		icons: ['data/images/contents/icon_m_tn.png'],
+		label: 'm, M'
+	},
+	chapter_s: {
+		type: 'consonant',
+		icons: ['data/images/contents/icon_s_tn.png'],
+		label: 's, S'
+	},
+	chapter_t: {
+		type: 'consonant',
+		icons: ['data/images/contents/icon_t_tn.png'],
+		label: 't, T'
+	},
+	chapter_v: {
+		type: 'consonant',
+		icons: ['data/images/contents/icon_v_tn.png'],
+		label: 'v, V'
+	},
+	chapter_recap_1: {
+		type: 'system',
+		icons: ['data/images/contents/icon_pages_tn.png'],
+		label: 'Ismétlés (A–V)'
+	},
+	chapter_test_1: {
+		type: 'system',
+		icons: ['data/images/contents/icon_write_tn.png'],
+		label: 'Mit tudok? (1.)'
+	},
+	chapter_e: {
+		type: 'vowel',
+		icons: ['data/images/contents/icon_e_tn.png'],
+		label: 'e, E'
+	},
+	chapter_l: {
+		type: 'consonant',
+		icons: ['data/images/contents/icon_l_tn.png'],
+		label: 'l, L'
+	},
+	chapter_u_uacc: {
+		type: 'vowel',
+		icons: ['data/images/contents/icon_u_tn.png','data/images/contents/icon_uacc_tn.png'],
+		label: 'u, U, ú, Ú'
+	},
+	chapter_p: {
+		type: 'consonant',
+		icons: ['data/images/contents/icon_p_tn.png'],
+		label: 'p, P'
+	},
+	chapter_aacc: {
+		type: 'vowel',
+		icons: ['data/images/contents/icon_aacc_tn.png'],
+		label: 'á, Á'
+	},
+	chapter_c: {
+		type: 'consonant',
+		icons: ['data/images/contents/icon_c_tn.png'],
+		label: 'c, C'
+	},
+	chapter_k: {
+		type: 'consonant',
+		icons: ['data/images/contents/icon_k_tn.png'],
+		label: 'k, K'
+	},
+	chapter_f: {
+		type: 'consonant',
+		icons: ['data/images/contents/icon_f_tn.png'],
+		label: 'f, F'
+	},
+	chapter_recap_2: {
+		type: 'system',
+		icons: ['data/images/contents/icon_pages_tn.png'],
+		label: 'Ismétlés (A–F)'
+	},
+	chapter_test_2: {
+		type: 'system',
+		icons: ['data/images/contents/icon_write_tn.png'],
+		label: 'Mit tudok? (2.)'
+	},
+	help: {
+		type: 'system',
+		icons: ['data/images/contents/icon_book_tn.png'],
+		label: 'Segédlet'
+	},
+	chapter_dev: {
+		type: 'system',
+		icons: ['data/images/contents/icon_pages_tn.png'],
+		label: 'Developer Map'
+	}
+}
